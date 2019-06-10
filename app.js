de3489d13014476998b59124ab232f79a221ab5d
Shashank Jaiswal <shashankjaiswal@dyn-160-39-224-150.dyn.columbia.edu>
2019-06-10 19:44:02 -0400

    basic site is working , game will correctly output

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //  time remining in countdown, once it hits 0, end the quiz
-var timeRemaining = 15;
+var timeRemaining = 10;
 
 // variable to store a correct answer
 var correctAnswers = 0;
@@ -7,9 +7,32 @@ var correctAnswers = 0;
 // function to get rid of questions and end the game
 function endGame() {
    // var questions =  $("#questions");
+
+   for (let i = 1; i < 3; i++) {
+   var radios = document.getElementsByName("choices" + i);
+  // console.log(radios);
+
+ //for loop to go through each array 
+ //checking the "user input" with the correct value
+ // radio.checked it acts like a boolen statement
+ //updating the correct answer and displaying it after
+  for (let j = 0; j < radios.length; j++) {
+      var radio = radios[j];
+      if (radio.value === "correct" && radio.checked) {
+          correctAnswers++;
+      }
+     
+      
+  }
+ }
+
+
+
+   
     $("#questions").empty();
     $("#questions").append("Correct Answers :"+ correctAnswers);
     $("#questions").append("<br>");
+    //incorrect answers = total questions -correct answers.
     $("#questions").append("InCorrect Answers :"+ (2- correctAnswers));
     
 }
